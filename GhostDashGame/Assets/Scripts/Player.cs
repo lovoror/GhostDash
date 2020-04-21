5254715169f931972bedee8f0f05b7fa8cbab907
louis1642 <62564969+louis1642@users.noreply.github.com>
2020-04-21 17:03:18 +0200

    added movement scripts
    in Player.cs and PlayerController.cs

---
+++ GhostDashGame/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(PlayerController))]
+public class Player : MonoBehaviour {
+
+    public float moveSpeed = 4;
+
+    PlayerController playerController;
+
+    private void Awake() {
+        playerController = GetComponent<PlayerController>();
+    }
+
+
+    void Update() {
+        //  MOVEMENT INPUT
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Vector2 moveVelocity = moveInput.normalized * moveSpeed;
+
+        playerController.Move(moveVelocity);
+    }
+}
---
+++ GhostDashGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody2D rb;
+    Vector2 _velocity;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void Move(Vector3 velocity) {
+        _velocity = velocity;
+    }
+
+
+    void FixedUpdate() {
+        rb.MovePosition(rb.position + _velocity * Time.fixedDeltaTime);
+    }
+}
