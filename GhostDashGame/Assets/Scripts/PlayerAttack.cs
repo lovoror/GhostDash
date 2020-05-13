71f2f9b41d77856e463500ce49f309aced4fe66f
louis1642 <62564969+louis1642@users.noreply.github.com>
2020-05-13 18:53:51 +0200

    implemented attack basis

---
+++ GhostDashGame/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour {
+
+    //Animator animator;
+    //public float attackTypeTreshold = .5f;
+
+
+    public Transform attackPoint;
+    public float attackRange = .5f;
+    public LayerMask enemyLayers;
+
+
+    void Awake() {
+        //animator = GetComponent<Animator>();
+    }
+
+
+    void Update() {
+
+        if (Input.GetButtonDown("Attack1")) {
+            Attack();
+        }
+
+
+    }
+
+    private void Attack() {
+
+        // animator.SetTrigger("Attack");
+
+        // detect enemies in range
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        // damage the first?
+        if (hitEnemies.Length > 0) {
+            print("Hit " + hitEnemies[0].name);
+        }
+        // foreach to damage all
+
+
+    }
+
+    private void OnDrawGizmosSelected() {
+        if (attackPoint != null) {
+            Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+        }
+    }
+}
