352622a01f919e79986d2f74400fea59efe89a40
louis1642 <62564969+louis1642@users.noreply.github.com>
2020-05-14 20:14:04 +0200

    Added projectile idea

---
+++ GhostDashGame/Assets/Scripts/Projectile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    public LayerMask collisionMask;
+    public float projectileSpeed = 10;
+
+    public float lifetime = 5;
+
+    void Start() {
+        Invoke("Deactivate", lifetime);
+    }
+
+
+    void Update() {
+        
+    }
+
+    void SetSpeed(float newSpeed) {
+        projectileSpeed = newSpeed;
+    }
+
+    void Deactivate() {
+        gameObject.SetActive(false);
+    }
+
+}
